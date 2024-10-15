4f119840f55e63a5e2159cdcbb4dffff58f0fae2
Promestromae <nzuvameshack@gmail.com>
2024-10-15 17:13:01 +0300

    Lonewolf

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask
-from bank import bank_bp
-from models import db
+from flask import Flask, render_template
 
-app = Flask(__name__)
-app.register_blueprint(bank_bp)
+def create_app():
+    app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank_system.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
+    @app.route('/')
+    def home():
+        return render_template('index.html')  # Render the index.html template
 
-@app.route('/')
-def home():
-    return "Welcome to the Excity Bank!"
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
